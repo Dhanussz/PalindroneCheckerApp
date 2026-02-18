29df20b08e315e1d9186aa440ff461fc785c8779
DD <dhanusxz@gmail.com>
2026-02-18 15:44:02 +0530

    DHANUSH KUMAR Updated Usecase1 Dynamic Palindrome Code

--- App/src/PalindromeCheckerApp.java
+++ App/src/PalindromeCheckerApp.java
@@ -4,7 +4,6 @@ public class PalindromeCheckerApp {
 
     public static void main(String[] args) {
 
-        // Usecase 1 : Dynamic Palindrome (User Input)
         Scanner sc = new Scanner(System.in);
 
         System.out.print("Enter a word: ");
@@ -12,11 +11,11 @@ public class PalindromeCheckerApp {
 
         String reverse = "";
 
-        for(int i = input.length() - 1; i >= 0; i--) {
+        for (int i = input.length() - 1; i >= 0; i--) {
             reverse = reverse + input.charAt(i);
         }
 
-        if(input.equals(reverse)) {
+        if (input.equals(reverse)) {
             System.out.println("Palindrome");
         } else {
             System.out.println("Not Palindrome");
