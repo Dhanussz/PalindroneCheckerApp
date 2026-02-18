e103889173df95173d780638cd95088a9dfb03e3
DD <dhanusxz@gmail.com>
2026-02-18 15:36:24 +0530

    Updated code in PNQ branch

--- App/src/PalindromeCheckerApp.java
+++ App/src/PalindromeCheckerApp.java
@@ -1,6 +1,27 @@
-public class PalindromeCheckerApp {
-    public static void main (String[] args ){
-        system.out.println ("welcome to palindrome checker app managent system")
+import java.util.Scanner;
 
+public class PalindromeCheckerApp {
+
+    public static void main(String[] args) {
+
+        // Usecase 1 : Dynamic Palindrome (User Input)
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a word: ");
+        String input = sc.nextLine();
+
+        String reverse = "";
+
+        for(int i = input.length() - 1; i >= 0; i--) {
+            reverse = reverse + input.charAt(i);
+        }
+
+        if(input.equals(reverse)) {
+            System.out.println("Palindrome");
+        } else {
+            System.out.println("Not Palindrome");
+        }
+
+        sc.close();
     }
 }
